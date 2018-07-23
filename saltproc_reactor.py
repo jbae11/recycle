df255514c7ab791960ef8a134cbeca0cfa971858
Jin Whan Bae <jbae11@illinois.edu>
2018-07-23 12:21:37 -0400

    market interaction waste, fill, fissile

--- saltproc_reactor.py
+++ saltproc_reactor.py
@@ -30,6 +30,12 @@ class saltproc_reactor(Facility):
         uilabel="Fill Commodity"
     )
 
+    fill_recipe = ts.String(
+        doc="The recipe this facility will take in for fertile materials ",
+        tooltip="Fill Recipe",
+        uilabel="Fill Recipe"
+    )
+
     fissile_out_commod = ts.String(
         doc="The fissile commodity this facility will output",
         tooltip="Fissile commodity",
@@ -76,15 +82,14 @@ class saltproc_reactor(Facility):
         print('TIME IS %i' %self.context.time)
         self.indx = self.context.dt * (self.context.time - self.enter_time)
         self.indx = int(self.indx / self.saltproc_timestep)
-        self.indx = min(self.indx, len(self.waste_db[:, 0]))
-        
+        self.indx = min(self.indx, len(self.waste_db[:, 0])) + 1
+        print('INDX IS: %i' %self.indx)
         # push waste from db to waste_tank
         self.get_waste()
         # push fissile from db to fissile_tank
         self.get_fissile()
 
         self.get_fill_demand()
-
         self.prev_indx = self.indx
         
     def tock(self):
@@ -99,12 +104,14 @@ class saltproc_reactor(Facility):
             waste_dump += self.waste_db[t, :]
         # convert this into a dictionary
         for i, val in enumerate(waste_dump):
-            iso = self.isos[i].decode('utf8')
-            waste_dict[iso] = val
-        waste_mass = self.driver_mass * sum(waste_dict.values())
-        material = ts.Material.create(self, waste_mass, waste_dict)
-        self.waste_tank.push(material)
-        print('PUSHED %f kg of WASTE INTO WASTE TANK' %waste_mass)
+            if val != 0:
+                iso = self.isos[i].decode('utf8')
+                waste_dict[iso] = val
+        if bool(waste_dict):
+            waste_mass = self.driver_mass * sum(waste_dict.values())
+            material = ts.Material.create(self, waste_mass, waste_dict)
+            self.waste_tank.push(material)
+            print('PUSHED %f kg of WASTE INTO WASTE TANK' %waste_mass)
 
     def get_fissile(self):
         fissile_dump = np.zeros(self.num_isotopes)
@@ -113,16 +120,17 @@ class saltproc_reactor(Facility):
         for t in np.arange(self.prev_indx, self.indx):
             fissile_dump += self.fissile_db[t, :]
         for i, val in enumerate(fissile_dump):
-            if val == 0:
-                continue
-            iso = self.isos[i].decode('utf8')
-            fissile_dict[iso] = val
-        fissile_mass = self.blanket_mass * sum(fissile_dict.values())
-        material = ts.Material.create(self, fissile_mass, fissile_dict)
-        self.fissile_tank.push(material)
-        print('PUSHED %f kg of FISSILE MATERIAL INTO FISSILE TANK' %fissile_mass)
+            if val != 0:
+                iso = self.isos[i].decode('utf8')
+                fissile_dict[iso] = val
+        if bool(fissile_dict):
+            fissile_mass = self.blanket_mass * sum(fissile_dict.values())
+            material = ts.Material.create(self, fissile_mass, fissile_dict)
+            self.fissile_tank.push(material)
+            print('PUSHED %f kg of FISSILE MATERIAL INTO FISSILE TANK' %fissile_mass)
 
     def get_fill_demand(self):
+        self.get_fill = False
         blanket_demand_dump = np.zeros(self.num_isotopes)
         driver_demand_dump = np.zeros(self.num_isotopes)
         fill_dict = {}
@@ -132,15 +140,90 @@ class saltproc_reactor(Facility):
         # since the data is in negative:      
         driver_fill_mass = -1.0 * self.driver_mass * sum(driver_demand_dump)
         blanket_fill_mass = -1.0 * self.blanket_mass * sum(blanket_demand_dump)
-        if (driver_fill_mass + blanket_fill_mass) == 0:
+        self.qty = driver_fill_mass + blanket_fill_mass
+        if (self.qty) == 0:
             return 0 
-        total_fill_demand = -1.0 * (blanket_demand_dump + driver_demand_dump)
-        for i, val in enumerate(total_fill_demand):
-            if val == 0:
-                continue
-            iso = self.isos[i].decode('utf8')
-            fill_dict[iso] = val
-        print('I WANT %f kg DEP U' %(driver_fill_mass+blanket_fill_mass))
-        demand_mat = ts.Material.create(self, driver_fill_mass + blanket_fill_mass,
-                                        fill_dict)
-        ### REQUEST MATERIAL OR STH
+        self.total_fill_demand = -1.0 * (driver_demand_dump + blanket_demand_dump) 
+        for i, val in enumerate(self.total_fill_demand):
+            if val != 0:
+                iso = self.isos[i].decode('utf8')
+                fill_dict[iso] = val
+        if bool(fill_dict):
+            print('I WANT %f kg DEP U' %(driver_fill_mass+blanket_fill_mass))
+            self.demand_mat = ts.Material.create(self, driver_fill_mass + blanket_fill_mass,
+                                            fill_dict)
+        if self.qty != 0.0:
+            self.get_fill = True
+
+    
+    def get_material_bids(self, requests):
+        """ Gets material bids that want its `outcommod' an
+            returns bid portfolio
+        """
+        bids = []
+        # waste commods
+        if self.waste_commod not in requests:
+            return
+        reqs = requests[self.waste_commod]
+        for req in reqs:
+            qty = min(req.target.quantity, self.waste_tank.quantity)
+            # returns if the inventory is empty
+            if self.waste_tank.empty():
+                return
+            # get the composition of the material next in line
+            next_in_line = self.waste_tank.peek()
+            mat = ts.Material.create_untracked(qty, next_in_line.comp())
+            bids.append({'request': req, 'offer': mat})
+        
+        # fissile commods
+        if self.fissile_out_commod not in requests:
+            return
+        reqs = requests[self.fissile_out_commod]
+        for req in reqs:
+            qty = min(req.target.quantity, self.fissile_tank.quantity)
+            if self.fissile_tank.empty():
+                return
+            next_in_line =self.fissile_tank.peek()
+            mat = ts.Material.create_untracked(qty, next_in_line.comp())
+            bids.append({'request': req, 'offer':mat})
+
+        port = {"bids": bids}
+        return port
+
+
+    def get_material_trades(self, trades):
+        """ Give out waste_commod and fissile_out_commod from
+            waste_tank and fissile_tank, respectively.
+        """
+        responses = {}
+        for trade in trades:
+            commodity = trade.request.commodity
+            if commodity == self.waste_commod:            
+                mat_list = self.waste_tank.pop_n(self.waste_tank.count)
+            if commodity == self.fissile_out_commod:
+                mat_list = self.fissile_tank.pop_n(self.fissile_tank.count)
+            # absorb all materials
+            # best way is to do it separately, but idk how to do it :(
+            for mat in mat_list[1:]:
+                mat_list[0].absorb(mat)
+            responses[trade] = mat_list[0]
+        return responses
+
+
+    def get_material_requests(self):
+        """ Ask for material fill_commod """
+        print(self.get_fill)
+        if self.get_fill:
+            fill_qty = self.qty
+            recipe = self.context.get_recipe(self.fill_recipe)
+            target = ts.Material.create_untracked(fill_qty, recipe)
+            commods = {self.fill_commod: target}
+            port = {'commodities': commods, 'constraints': fill_qty}
+            return port
+        else:
+            return {}
+
+    def accept_material_trades(self, responses):
+        """ Get fill_commod and store it into fill_tank """
+        for mat in responses.values():
+            self.fill_tank.push(mat)
